2b1082ba22c774efc44b2deb8a526bf0ecd7a040
Daniyal Shahrokhian <dani.shahrokhian@gmail.com>
2016-01-05 11:54:26 +0100

    Initial push of the demo, using rhill's implementation as core algorithm of the Voronoi Diagram
    The core algorithm will be later substituted by a custom-made out of the
    dual graph of the Delaunay Triangulation

---
+++ demo.js
@@ -0,0 +1,106 @@
+// Interface vars
+var bboxWidth = 800
+var	bboxHeight = 600
+
+var	bbox = {
+	xl: 0,
+	xr: this.bboxWidth,
+	yt: 0,
+	yb: this.bboxHeight
+};
+
+var colors = ["green","red"]
+
+// Core vars
+var	sites = []
+var	voronoi
+var	svg
+
+// Game vars
+var nTurns = 10
+var currentTurn = 1
+
+var VoronoiGame = {
+
+	init: function() {
+
+		voronoi = new Voronoi()
+
+		// Game box
+		svg = d3.select("body")
+		.append("svg")
+		.attr("width", window.bboxWidth)
+		.attr("height", window.bboxHeight)
+		.attr("style", "cursor:crosshair")
+		.attr("border", 2)
+		.on("click", function() {
+
+			var coordinates = d3.mouse(this)
+			VoronoiGame.addSite(coordinates)
+
+			if (++currentTurn > nTurns) {
+				alert("Game over")
+			}
+		})
+
+		svg.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", window.bboxWidth)
+		.attr("height", window.bboxHeight)
+		.style("stroke", "black")
+		.style("fill", "none")
+		.style("stroke-width", 2);
+
+	},
+
+	addSite: function(coordinates) {
+
+		var xVal = coordinates[0]
+		var yVal = coordinates[1]
+		
+		sites.push({x: xVal, y: yVal})
+		this.recompute(sites)
+	},
+
+	recompute : function(sites) {
+
+		// Clear svg
+		d3.select("svg").selectAll("line").remove()
+
+		// Recompute the diagram and draw it
+		this.drawSites(sites)
+		var diagram = window.voronoi.compute(sites, window.bbox)
+		this.drawEdges(diagram.edges)
+	},
+
+	drawSites : function(sites) {
+
+		for(var i = 0; i < sites.length; i++) {
+			
+			var site = sites[i]
+
+			window.svg.append("circle")
+			.attr("cx",site.x)
+			.attr("cy",site.y)
+			.attr("r", 4)
+			.attr("fill", colors[i%2]	)
+		}
+	},
+
+	drawEdges : function(edges) {
+
+		for(var i = 0; i < edges.length; i++) {
+
+			var edge = edges[i];
+
+			window.svg.append("line")
+			.attr("x1", edge.va.x)
+			.attr("y1", edge.va.y)
+			.attr("x2", edge.vb.x)
+			.attr("y2", edge.vb.y)
+			.attr("stroke", "black")
+			.attr("stroke-width", 2)
+		}
+	}
+}
