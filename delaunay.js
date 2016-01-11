05b3b80cb7966df32d4a4235942e4959f9b33202
Daniyal Shahrokhian <dani.shahrokhian@gmail.com>
2016-01-11 15:29:10 +0100

    Add skeleton files for core functionality (Voronoi diagrams and Delaunay triangulation). Add background music to the game interface.
    Until now we have been using rhill's implementation of the Voronoi
    Diagrams. This will be still used (temp folder) until we develop the
    custom Delaunay triangulation and Voronoi Diagram.

---
+++ delaunay.js
@@ -0,0 +1,34 @@
+var Delaunay;
+
+/* Usage: var delaunay = new Delaunay()
+		  var tirangles = delaunay.compute(sites)
+		  // The x value of the third point in the second triangle:
+		  var point = triangles[1].points[2].x
+ */
+Delaunay = {
+
+	/* Object used to represent the triangles form the Delaunay Triangulation
+ 	 * 
+	 * Usage: var triangle = new Triangle(points)
+	 *		  // The x value of the third point:
+	 *		  var point = triangle.p3.x 
+	 */
+	Triangle = function(points) {
+		this.p1 = points[0]
+		this.p2 = points[1]
+		this.p3 = points[2]
+	}
+
+	/* Generates the delaunay triangulation from the set of sites 
+	 * 
+	 * @parameter sites : the array of sites from which we want to generate the Delaunay Triangulation.
+	 * 					  Each element of this array is an object with the pattern
+	 						"{x,y}"
+	 					  This means that for accesing the x-value of the second site you should do
+	 					  	"sites[1].x"
+	 	@returns an array of Delaunay triangles. The points of the triangle are in counterclockwise order.
+	 */
+	compute: function(sites) {
+		/* TODO */
+	}
+}
---
+++ voronoi.js
@@ -0,0 +1,52 @@
+var Voronoi;
+
+/* Usage: var voronoi = new Voronoi()
+		  var faces = voronoi.compute(sites)
+		  // The x value of the third point in the second face:
+		  var point = faces[1].points[2].x
+ */
+Voronoi = {
+
+	/* Object used to represent the faces of the voronoi diagram
+	 * 
+	 * Usage: var face = new Face(points)
+     *   	  // The x value of the third point:
+     *        var point = face.points[2].x 
+     */
+	var Face = function(points) {
+		this.points = points
+	}
+
+	/* Generates the voronoi diagram from the set of sites 
+	 * 
+	 * @parameter sites : the array of sites from which we want to generate the Voronoi Diagram.
+	 * 					  Each element of this array is an object with the pattern
+	 						"{x,y}"
+	 					  This means that for accesing the x-value of the second site you should do
+	 					  	"sites[1].x"
+	 	@returns an array of Voronoi faces. The points of the face are in counterclockwise order.
+	 */
+	compute: function(sites) {
+		/* TODO */
+	}
+}
+
+/* Calculates the bisector given the segment defined by the Points
+p1 and p2*/
+function bisector(p1, p2) {
+
+	var mid_x = (p2.x + p1.x) / 2
+	var mid_y = (p2.y + p1.y) / 2
+	var slope = [(p2.y - p1.y), (p2.x - p1.x)]
+	var neg_rec_slope = -slope[1] / slope[0]
+
+	/* We use the line formula y = mx + b  to get the value b*/
+	var b = mid_y - neg_rec_slope * mid_x
+
+	var bisector = {
+		slope: neg_rec_slope,
+		y_axis_cut: b
+	};
+
+	return bisector;
+}
